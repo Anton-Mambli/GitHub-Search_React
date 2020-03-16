0b97f36616936b6ec63eb1109aaebd9d8cc2eff5
= <=>
2020-03-16 21:50:00 +0400

    Updates

---
+++ precache-manifest.c15f7ac34f7c094b36399fe33070a106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182a2900524d8d3a30dd6b1296e24dec",
+    "url": "/GitHub-Search_React/index.html"
+  },
+  {
+    "revision": "6b877cd275e5c11ec219",
+    "url": "/GitHub-Search_React/static/css/main.3920e8f8.chunk.css"
+  },
+  {
+    "revision": "442fb099cb84215ca65b",
+    "url": "/GitHub-Search_React/static/js/2.2b86f46a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/GitHub-Search_React/static/js/2.2b86f46a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b877cd275e5c11ec219",
+    "url": "/GitHub-Search_React/static/js/main.1bc4be6f.chunk.js"
+  },
+  {
+    "revision": "ba19292894979a5abcb8",
+    "url": "/GitHub-Search_React/static/js/runtime-main.777a32d1.js"
+  }
+]);
